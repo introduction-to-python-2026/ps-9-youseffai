adf52064982fc056ac62bf4ab7606ea386f1a9d6
youseffai <altyoxox@gmail.com>
2026-01-04 18:18:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import pandas as pd
+df=pd.read_csv("/content/parkinsons.csv")
+print(list(df.columns))
+selected_feature=["HNR","NHR"]
+x=df[selected_feature]
+y=df["status"]
 
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+x = scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors=4)
+model.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')

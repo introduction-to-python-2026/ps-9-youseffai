d89fc56e59860023b86ac6e9ec10137d1f9f2aa8
youseffai <altyoxox@gmail.com>
2026-01-04 18:21:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,6 @@ from sklearn.metrics import accuracy_score
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
+import joblib
+
+joblib.dump(model, 'my_model.joblib')
